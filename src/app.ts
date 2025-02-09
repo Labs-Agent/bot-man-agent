5c62b36eb57144acca3532abaa6efd7cab930883
vibhatsu <maulikbarot2915@gmail.com>
2025-02-09 06:15:52 +0530

    mod: global model instances
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express from "express";
 import { setAgentRoutes } from "./routes/agentRoutes";
 import { setWorkflowRoutes } from "./routes/workflowRoutes";
+import { setToolRoutes } from "./routes/toolRoutes";
+
+import { ToolModel } from "./models/tool";
+import { AgentModel } from "./models/agent";
+import { WorkflowModel } from "./models/workflow";
+
+const toolModel = new ToolModel();
+const agentModel = new AgentModel();
+const workflowModel = new WorkflowModel();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-setAgentRoutes(app);
-setWorkflowRoutes(app);
+setToolRoutes(app, toolModel);
+setAgentRoutes(app, agentModel, toolModel);
+setWorkflowRoutes(app, workflowModel, agentModel);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
